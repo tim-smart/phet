d7456620da33f13e890318d803f4c3595a885404
Tim <tim@fostle.com>
2009-11-24 02:49:20 +1300

    Removed test specific code

--- run.php
+++ run.php
@@ -5,11 +5,9 @@ require 'phet/phet.inc.php';
 
 require 'phet/modules/admin.module.php';
 require 'phet/modules/comet.module.php';
-require 'phet/modules/test.module.php';
 
 $phet->registerModule( new PhetModuleAdmin() );
 $phet->registerModule( new PhetModuleComet() );
-$phet->registerModule( new PhetModuleTest() );
 
 $phet->serve();
 
